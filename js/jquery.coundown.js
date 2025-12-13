fef2aa3e4d8922311ea63a499c8e25430ab30a23
Dong Tran <dong.tran.zj@renesas.com>
2025-12-13 22:26:21 +0700

    update day month year

--- js/jquery.coundown.js
+++ js/jquery.coundown.js
@@ -7,16 +7,16 @@
 (function ($) {
 	$.fn.countdown = function (options, callback) {
 		var settings = $.extend({
-			date: null,
-			offset: null,
-			day: 'Hari',
-			days: 'Hari',
-			hour: 'Jam',
-			hours: 'Jam',
-			minute: 'Menit',
-			minutes: 'Menit',
-			second: 'Detik',
-			seconds: 'Detik'
+		date: null,
+		offset: null,
+		day: 'Day',
+		days: 'Days',
+		hour: 'Hour',
+		hours: 'Hours',
+		minute: 'Minute',
+		minutes: 'Minutes',
+		second: 'Second',
+		seconds: 'Seconds'
 		}, options);
 
 		// Throw error if date is not set
